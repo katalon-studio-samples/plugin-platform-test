cf85b8877961b8c17c9886f3b362d29ebac81313
linhdoo <linhtkdo@kms-technology.com>
2021-07-30 13:49:04 +0700

    Initial commit

--- src/main/java/com/mycompany/plugin/MyExecutionEventListener.java
+++ src/main/java/com/mycompany/plugin/MyExecutionEventListener.java
@@ -2,10 +2,15 @@ package com.mycompany.plugin;
 
 import org.osgi.service.event.Event;
 
+import com.katalon.platform.api.controller.ReportController;
 import com.katalon.platform.api.event.EventListener;
 import com.katalon.platform.api.event.ExecutionEvent;
+import com.katalon.platform.api.exception.ResourceException;
 import com.katalon.platform.api.execution.TestSuiteExecutionContext;
 import com.katalon.platform.api.extension.EventListenerInitializer;
+import com.katalon.platform.api.model.ReportEntity;
+import com.katalon.platform.api.service.ApplicationManager;
+
 public class MyExecutionEventListener implements EventListenerInitializer {
 
     @Override
@@ -17,7 +22,20 @@ public class MyExecutionEventListener implements EventListenerInitializer {
                 TestSuiteExecutionContext testSuiteContext = (TestSuiteExecutionContext) eventObject
                         .getExecutionContext();
 
-                System.out.println("Test execution completed: " + testSuiteContext.getReportId());
+                String reportId = testSuiteContext.getReportId();
+
+                ReportController ctl = ApplicationManager.getInstance()
+                        .getControllerManager()
+                        .getController(ReportController.class);
+
+                try {
+                    ReportEntity report = ctl.getReport(null, reportId);
+                    System.out
+                            .println("Test execution completed - Junit file path: " + report.getJunitReportLocation());
+
+                } catch (ResourceException e) {
+                    System.out.println(e.getDetailMessage());
+                }
             }
         });
     }
--- src/main/java/com/mycompany/plugin/MyPluginActivationListener.java
+++ src/main/java/com/mycompany/plugin/MyPluginActivationListener.java
@@ -1,11 +1,21 @@
 package com.mycompany.plugin;
 
 import com.katalon.platform.api.Plugin;
+import com.katalon.platform.api.controller.ExecutionController;
 import com.katalon.platform.api.extension.PluginActivationListener;
+import com.katalon.platform.api.service.ApplicationManager;
 
 public class MyPluginActivationListener implements PluginActivationListener {
     @Override
     public void afterActivation(Plugin plugin) {
         System.out.println("Hello, my plugin is: " + plugin.getPluginId());
+
+        ExecutionController ctl = ApplicationManager.getInstance()
+                .getControllerManager()
+                .getController(ExecutionController.class);
+
+        System.out.println("Running mode: " + ctl.getRunningMode());
+
+        System.out.println("JRE location: " + ctl.getJreLocation());
     }
 }
