19b8bb1e027859b0247bfbf3b1c4515653d4bc48
linhdoo <linhtkdo@kms-technology.com>
2021-08-03 14:52:09 +0700

    Add Test for test suite collection report

--- src/main/java/com/mycompany/plugin/MyExecutionEventListener.java
+++ src/main/java/com/mycompany/plugin/MyExecutionEventListener.java
@@ -2,12 +2,15 @@ package com.mycompany.plugin;
 
 import org.osgi.service.event.Event;
 
+import com.katalon.platform.api.controller.ReportCollectionController;
 import com.katalon.platform.api.controller.ReportController;
 import com.katalon.platform.api.event.EventListener;
 import com.katalon.platform.api.event.ExecutionEvent;
 import com.katalon.platform.api.exception.ResourceException;
+import com.katalon.platform.api.execution.TestSuiteCollectionExecutionContext;
 import com.katalon.platform.api.execution.TestSuiteExecutionContext;
 import com.katalon.platform.api.extension.EventListenerInitializer;
+import com.katalon.platform.api.model.ReportCollectionEntity;
 import com.katalon.platform.api.model.ReportEntity;
 import com.katalon.platform.api.service.ApplicationManager;
 
@@ -30,8 +33,29 @@ public class MyExecutionEventListener implements EventListenerInitializer {
 
                 try {
                     ReportEntity report = ctl.getReport(null, reportId);
-                    System.out
-                            .println("Test execution completed - Junit file path: " + report.getJunitReportLocation());
+                    System.out.println(
+                            "Test suite execution completed - Junit file path: " + report.getJunitReportLocation());
+
+                } catch (ResourceException e) {
+                    System.out.println(e.getDetailMessage());
+                }
+
+            } else if (ExecutionEvent.TEST_SUITE_COLLECTION_FINISHED_EVENT.equals(event.getTopic())) {
+                ExecutionEvent eventObject = (ExecutionEvent) event.getProperty("org.eclipse.e4.data");
+
+                TestSuiteCollectionExecutionContext testSuiteCollectionContext = (TestSuiteCollectionExecutionContext) eventObject
+                        .getExecutionContext();
+
+                String path = testSuiteCollectionContext.getReportFolderLocation();
+
+                ReportCollectionController ctl = ApplicationManager.getInstance()
+                        .getControllerManager()
+                        .getController(ReportCollectionController.class);
+
+                try {
+                    ReportCollectionEntity report = ctl.getReport(path);
+                    System.out.println("Test suite collection execution completed - Junit file path: "
+                            + report.getJunitReportLocation());
 
                 } catch (ResourceException e) {
                     System.out.println(e.getDetailMessage());
